6f0590c982f23c8d44338871345810d3148591dc
Gonzalo Perez Ramos <GP249454@fi365.ort.edu.uy>
2022-11-17 19:07:40 -0300

    borro juego prueba 2 inutilizado

--- src/sistemaDistribucion/Main.java
+++ src/sistemaDistribucion/Main.java
@@ -9,15 +9,5 @@ public class Main {
         Pruebas p = new Pruebas (obl);
         p.probar(); 
     }
-        public static void juegodeprueba2(Obligatorio obl, Prueba p) throws ParseException {
-        // escriba su juego de prueba aqui
-        int MAX_CANT_PALABRAS_X_LINEA = 4;
-        
-        System.out.println( "------------------------------------------------");
-        
-        p.ver(obl.crearSistemaDeDistribucion(MAX_CANT_PALABRAS_X_LINEA).resultado, Retorno.Resultado.OK, "Se creara sistema para 3 palabras por linea");
-        p.ver(obl.listarClientesOrdenado().resultado, Retorno.Resultado.OK, "Se listan los clientes");   
-        
-        
-        }    
+   
 }
